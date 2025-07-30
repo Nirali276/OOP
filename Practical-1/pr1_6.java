74d65fa72b184461d7c8bb7f3899ea5a149f84e4
Nirali276 <niralibhavsar68@gmail.com>
2025-07-30 22:49:06 +0530

    Create pr1_6.java

---
+++ Practical-1/pr1_6.java
@@ -0,0 +1,9 @@
+public class pr1_6
+{
+public static void main(String[] agrs)
+{
+System.out.println("#######################");
+System.out.println("# 'Keep Learning Java' #");
+System.out.println("#######################");
+}
+}
