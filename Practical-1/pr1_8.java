f0dbca1b6cb4143ce7b369b7c7748253d8192f01
Nirali276 <niralibhavsar68@gmail.com>
2025-07-30 22:49:57 +0530

    Create pr1_8.java

---
+++ Practical-1/pr1_8.java
@@ -0,0 +1,10 @@
+public class pr1_8
+{
+public static void main(String[] args)
+{
+double celsius = Double.parseDouble(args[0]);
+double fahrenheit = (celsius * 1.8) + 32;
+System.out.println("celsius:" + celsius);
+System.out.println(" fahrenheit = " + fahrenheit);
+}
+}
