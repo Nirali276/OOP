24341e39ebe7dc5cccd2691d59f60dfb89def7a9
Nirali276 <niralibhavsar68@gmail.com>
2025-07-30 22:46:50 +0530

    Create pr1_3.java

---
+++ Practical-1/pr1_3.java
@@ -0,0 +1,9 @@
+public class pr1_3
+{
+public static void main(String[] args)
+{
+System.out.println("#######################");
+System.out.println("# Java programming lab #guni@200$$s");
+System.out.println("#######################");
+}
+}
