0a3706897b5cfc042376f6cb94b53da32609409d
Nirali276 <niralibhavsar68@gmail.com>
2025-08-05 09:29:53 +0530

    Create pr2_2.java

---
+++ Practical-2/pr2_2.java
@@ -0,0 +1,14 @@
+public class pr2_2
+{
+public static void main(String args[])
+{
+double a=Double.parseDouble(args[0]);
+double b=Double.parseDouble(args[1]);
+
+System.out.println("a+b="+ (a+b));
+System.out.println("a-b="+ (a-b));
+System.out.println("a*b="+ (a*b));
+System.out.println("a/b="+ (a/b));
+System.out.println("a%b="+ (a%b));
+}
+}
