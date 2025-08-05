46247d04f6032b715010209d26941b2c590c97dc
Nirali276 <niralibhavsar68@gmail.com>
2025-08-05 09:28:47 +0530

    Create pr2_1.java

---
+++ Practical-2/pr2_1.java
@@ -0,0 +1,29 @@
+public class pr2_1
+{
+	public static void main(String args[])
+	{
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+ 
+		
+		if(a > b && a > c)
+		{
+		 System.out.println("A:" + a);
+		 System.out.println("A is maximum");
+		}
+		else if(b > a && b > c)
+		{
+		 System.out.println("B:" + b);
+		 System.out.println("B is maximum");
+		}
+		else if(c > a && c > b)
+		{
+		 System.out.println("C:" + c);
+		 System.out.println("B is maximum");
+		}
+		else{
+			System.out.println("All are equals");
+		}
+	}
+}
