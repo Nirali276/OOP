569c78b32dbcbea78a442c4173cb9191471cc856
Nirali276 <niralibhavsar68@gmail.com>
2025-08-24 15:19:37 +0530

    Update pr2_2.java

--- Practical-2/pr2_2.java
+++ Practical-2/pr2_2.java
@@ -5,10 +5,10 @@ public static void main(String[] args)
 double a=Double.parseDouble(args[0]);
 double b=Double.parseDouble(args[1]);
 
-System.out.println("a+b="+ (a+b));
-System.out.println("a-b="+ (a-b));
-System.out.println("a*b="+ (a*b));
-System.out.println("a/b="+ (a/b));
-System.out.println("a%b="+ (a%b));
+System.out.println("sum="+ (a+b));
+System.out.println("difference="+ (a-b));
+System.out.println("product="+ (a*b));
+System.out.println("quotient="+ (a/b));
+System.out.println("modulus="+ (a%b));
 }
 }
