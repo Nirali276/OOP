cc960389521fb20db9db6fcaf034887413ce4e89
Nirali276 <niralibhavsar68@gmail.com>
2025-08-24 14:35:50 +0530

    Create pr2_5.java

---
+++ Practical-2/pr2_5.java
@@ -0,0 +1,14 @@
+public class pr2_5
+{
+public static void main(String[] args)
+{
+   int number = Integer.parseInt(args[0]);
+   int sum = 0;
+
+ while (number != 0){
+    sum += number % 10;
+    number /= 10;
+} 
+System.out.println("Sum of digits = " + sum);
+}  
+}
